e533c0e383232c014327eff53a9733e2747f0464
Geoffrey Gaillard <ggaillard@live.fr>
2016-01-28 15:49:16 +0100

    Machine menu

--- src/cafe/Machine.java
+++ src/cafe/Machine.java
@@ -5,12 +5,47 @@
  */
 package cafe;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
- *
  * @author maxime
  */
 public class Machine {
+    
+    private static String menu = ""
+            + "\n 1 - Acheter une boisson"
+            + "\n 2 - Ajouter une boisson"
+            + "\n 3 - Modifier une boisson"
+            + "\n 4 - Supprimer une boisson"
+            + "\n 5 - Ajouter un ingredient"
+            + "\n 6 - Afficher le stock d'ingredients"
+            + "\n 7 - Quitter."
+            + "\n ";
+
     public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
+        Scanner sc = new Scanner(System.in);
+        Integer choix = 0;
+        do{
+            System.out.println(menu);
+            System.out.println("=> ");
+            
+            try{
+                choix = sc.nextInt();
+            }catch(InputMismatchException e){
+                continue;
+            }
+            
+            
+            switch(choix){
+                case 5:
+                    stock.StockIngredient.getStock().ajouterIngredient();
+                    break;
+                default:
+                                             
+            }
+            
+        } while(choix != 7);
+    }
+    
 }
