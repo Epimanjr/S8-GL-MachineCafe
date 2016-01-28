dcf0df8f3c8e79bc73302c691e740ae0a7c6d39d
maxime <maxime@maxime-LDLC15>
2016-01-28 15:03:51 +0100

    Ajout du stock des ingrédients

--- src/stock/StockIngredient.java
+++ src/stock/StockIngredient.java
@@ -1,5 +1,7 @@
-
 package stock;
+
+import cafe.Ingredient;
+import java.util.HashMap;
 
 /**
  *
@@ -7,5 +9,42 @@ package stock;
  * @author Geoffrey GAILLARD
  */
 public class StockIngredient {
+
+    /**
+     * Liste des ingrédients avec son stock.
+     */
+    private final HashMap<Ingredient, Integer> ingredients;
+
+    private static final StockIngredient stock = new StockIngredient();
+
+    /**
+     * Initialise les stock
+     */
+    private StockIngredient() {
+        // Initialise la map avec toutes les valeurs de l'énumération
+        ingredients = new HashMap<>();
+        for (Ingredient i : Ingredient.values()) {
+            ingredients.put(i, 0);
+        }
+    }
+
+    /**
+     * Récupère le stock d'ingrédients.
+     *
+     * @return le stock
+     */
+    public static StockIngredient getStock() {
+        return stock;
+    }
     
+    /**
+     * Méthode qui ajoute un ingrédient.
+     */
+    public void ajouterIngredient() {
+        //TODO
+    }
+    
+    public void ajouterIngredient(Ingredient i, int quantite) {
+        //TODO
+    }
 }
