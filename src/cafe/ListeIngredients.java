ef3eea0d09567a6043eabe9d1dfa8c6c4ff1baac
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-02-03 14:49:05 +0100

    Liste d'ingrédients

---
+++ src/cafe/ListeIngredients.java
@@ -0,0 +1,30 @@
+package cafe;
+
+import java.util.ArrayList;
+
+public class ListeIngredients extends ArrayList<Ingredient> {
+
+    /**
+     * Notre liste
+     */
+    private static ListeIngredients liste = new ListeIngredients();
+
+    private ListeIngredients() {
+
+    }
+
+    /**
+     * Permet de récupérer notre liste d'ingrédients à partir de l'extérieur.
+     */
+    public static ListeIngredients getListe() {
+        return liste;
+    }
+
+    private void init() {
+        this.add(new Ingredient("Café"));
+        this.add(new Ingredient("Lait"));
+        this.add(new Ingredient("Sucre"));
+        this.add(new Ingredient("Chocolat"));
+        this.add(new Ingredient("Thé"));
+    }
+}
